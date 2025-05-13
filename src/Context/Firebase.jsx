c8453e0983e922abcfbc6999643e4967d5604f3a
RifatParadoxical <zikkiz275@gmail.com>
2025-05-14 05:46:11 +0600

    updated heading and console

--- src/Context/Firebase.jsx
+++ src/Context/Firebase.jsx
@@ -16,7 +16,6 @@ const firebaseConfig = {
   };
 const appInitial = initializeApp(firebaseConfig)
 export const auth = getAuth(appInitial)
- console.log(firebaseConfig)
 
 //exporting components & modules.
 const AppContext = createContext();
@@ -31,67 +30,3 @@ return(
 )
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { createContext } from "react";
-// import { initializeApp } from "firebase/app";
-// import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCuevlpqhXZtvBh8aNzieVbv0tPearzAuE",
-//     authDomain: "tactoctoe-ddd39.firebaseapp.com",
-//     projectId: "tactoctoe-ddd39",
-//     storageBucket: "tactoctoe-ddd39.firebasestorage.app",
-//     messagingSenderId: "942469692843",
-//     appId: "1:942469692843:web:5c4a1bf07d2dcb183e66ee",
-//     measurementId: "G-YSZ8LFJX86"
-//   };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app)
-
-// export const userContext = createContext();
-// export const AppProvider = ({children}) =>{
-//     const SignUpWithEmailAndPassword = (email, password) => {
-//        return createUserWithEmailAndPassword(auth, email, password)
-//     }
-//     return(
-//         <userContext.Provider value={ SignUpWithEmailAndPassword }>
-//             {children}
-//         </userContext.Provider>
-//     )
-// }
